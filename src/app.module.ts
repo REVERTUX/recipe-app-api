7d6b70f1a3df0d80e51d4f47b70c423167439c89
revertux <revertux@gmail.com>
2023-04-25 14:46:25 +0200

    Fix app because of removed module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,9 @@ import { PrismaModule } from './prisma/prisma.module';
 import { RecipesModule } from './recipes/recipes.module';
 import { ReviewsController } from './reviews/reviews.controller';
 import { ReviewsService } from './reviews/reviews.service';
-import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ReviewsModule, PrismaModule, RecipesModule, UsersModule],
+  imports: [ReviewsModule, PrismaModule, RecipesModule],
   controllers: [AppController, RecipesController, ReviewsController],
   providers: [AppService, RecipesService, ReviewsService],
 })
